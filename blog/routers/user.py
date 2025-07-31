633f4b3ebfc5937da558e52679bbc78bba946376
Dhinesh <dhinesh342@gmail.com>
2025-08-01 00:07:27 +0530

    Routers,service folders,router,relationships

---
+++ blog/routers/user.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter,Depends,status,Response,HTTPException
+from typing import List
+from sqlalchemy.orm import Session
+from .. import schemas,database,models
+# from ..hashing import Hash
+from ..services import user
+
+router=APIRouter(
+    prefix="/user",
+    tags=["users"]
+)
+get_db=database.get_db
+
+@router.post("/",response_model=schemas.ShowUser)
+def create_user(request:schemas.User,db:Session=Depends(get_db)):
+    return user.create(request,db)
+
+@router.get("/{id}",response_model=schemas.ShowUser)
+def get_user(id:int,db:Session=Depends(get_db)):
+    return user.get(id,db)
--- blog/schemas.py
+++ blog/schemas.py
@@ -1,6 +1,27 @@
 from pydantic import BaseModel
+from typing import List
 
 
 class Blog(BaseModel):
     title:str
     body:str
+
+
+
+class User(BaseModel):
+    name:str
+    email:str
+    password:str
+
+class ShowUser(BaseModel):
+    name:str
+    email:str
+    blogs:List[Blog]
+
+class showBlog(BaseModel):
+    title:str   
+    body:str 
+    creator:ShowUser
+    # class Config():
+    #     orm_mode=True
+
